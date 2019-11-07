023b67e407f6574ffef8d7906314745a50e807d7
ZhouLiDong <45483980+ZhouLiDong@users.noreply.github.com>
2019-11-07 16:16:17 +0800

    Update TypeReference.java

--- src/main/java/com/bean/valid/util/TypeReference.java
+++ src/main/java/com/bean/valid/util/TypeReference.java
@@ -5,21 +5,10 @@ import java.lang.reflect.Type;
 import java.util.List;
 
 /**
- * 使用时继承该类，传入泛型参数即可
+ *  使用该类获取泛型信息
  * @Author 周利东
  * @Date: 2019/10/25 9:16
  */
 public class TypeReference<T> {
 
-//    private final Type type;
-//
-//    protected TypeReference(){
-//        Type superClass = getClass().getGenericSuperclass();
-//
-//        type = ((ParameterizedType) superClass).getActualTypeArguments()[0];
-//    }
-//
-//    public Type getType() {
-//        return type;
-//    }
 }
